ce9bcac568023733759e600656f036c65ae31178
nimitt <nimittsethiya@gmail.com>
2020-10-21 14:03:16 +0530

    tempdata

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -402,17 +402,9 @@ class Products extends Admin_Controller
             //     'availability' => $this->input->post('availability'),
             // );
 
-            $tax_data = $this->model_tax->getTaxFromID($this->input->post('tax'));
-            $unit_data = $this->model_unit->getUnitFromID($this->input->post('unit'));
-
             $data = array(
-<<<<<<< HEAD
                 // 'Item_ID' => $this->input->post('item_id'),
                 'Category_ID' => json_encode($this->input->post('category')) ,
-=======
-                // 'Item_ID' => $this->input->post('item_code'),
-                'Category_ID' => $this->input->post('category'),
->>>>>>> fb7e030b974fac12a4f405a4a15b6822cb2decab
                 'Company_ID' => $this->input->post('store'),
                 'Item_Name' => $this->input->post('product_name'),
         		'Item_Make' => $this->input->post('make'),
@@ -420,13 +412,12 @@ class Products extends Admin_Controller
         		'Price' => $this->input->post('list_price'),
                 'Item_Code' => $this->input->post('item_code'),
                 'Pack_Size' => $this->input->post('pack_size'),
-                'Tax' => $tax_data['sValue'],
-                'iTax_ID' => $tax_data['iTax_ID'],
+                'Tax' => $this->input->post('tax'),
                 'Purchase_Price' => $this->input->post('purchase_rate'),
                 'Opening_Balance' => $this->input->post('opening_balance'),
                 'Opening_Balance' => $this->input->post('opening_balance'),
                 'Current_Balance' => $this->input->post('qty'),
-                'iTax_ID' => json_encode($this->input->post('iTax_ID')) ,
+                'iTax_ID' => json_encode($this->input->post('iTax_ID')),
                 'Item_Description' => $this->input->post('description'),
                 'ReOrder_Level' => $this->input->post('reorder_level'),
                 'Max_Suggested_Qty' => $this->input->post('qty'),
@@ -444,12 +435,12 @@ class Products extends Admin_Controller
         	);
 
             
-            // if($_FILES['product_image']['size'] > 0) {
-            //     $upload_image = $this->upload_image();
-            //     $upload_image = array('image' => $upload_image);
+            if($_FILES['product_image']['size'] > 0) {
+                $upload_image = $this->upload_image();
+                $upload_image = array('image' => $upload_image);
                 
-            //     $this->model_products->update($upload_image, $product_id);
-            // }
+                $this->model_products->update($upload_image, $product_id);
+            }
 
             $update = $this->model_products->update($data, $product_id);
             if($update == true) {
@@ -487,8 +478,7 @@ class Products extends Admin_Controller
             
             $this->data['unit_data']= $this->model_unit->getUnitData();
             $this->data['tax_data']= $this->model_tax->getTaxData();
-            $this->data['tax_data_id']= $this->model_tax->getTaxFromID($product_data['iTax_ID']);
-           
+
             $this->data['unit_data_sUnit']= $this->model_unit->getUnitDataFromsUnit($product_data['sUnit']);
 
             $this->render_template('products/edit', $this->data); 
