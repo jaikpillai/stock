f6f2a9ebf61f4667371d111d1e6239c324936681
Jai Pillai <jaipilllai@gmail.com>
2020-11-23 12:39:41 +0530

    deploy test 2

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+//Development
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'stock',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
+//Remote DB
+$dsn = "mysql:host=remotemysql.com;dbnam=vAcWtXMOgZ;charset=utf8";
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'stock',
+	'hostname' => 'remotemysql.com',
+	'username' => 'vAcWtXMOgZ',
+	'password' => 'Ty4sY5QTCa',
+	'database' => 'vAcWtXMOgZ',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
