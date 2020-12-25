e53e2997ef1706b526806b4a956a45c6c34ce58a
unknown <jaipilllai@gmail.com>
2020-12-25 13:44:39 +0530

    financial drop down populate

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -50,13 +50,14 @@
                           $current_date = date("Y-m-d");
                           $start_date = $v['start_date'];
                           $end_date = $v['end_date'];
-                          $financial_year_id = $financial_year_id = $v['key_value'];
+                          $financial_year_id = $v['key_value'];
 
-                          if (($current_date >= $start_date) && ($current_date <= $end_date)){ ?>
+                          if (($current_date >= $start_date) ){ ?>
 
                       <option value="<?php echo $v['key_value'] ?>" <?php if (($v['key_value'] == $this->session->userdata("selected_financial_year"))) {
                                                                       echo 'selected="selected"';
-                                                                    } }?>><?php echo $v['year_range'] ?></option>
+                                                                    }}?>><?php echo $v['year_range'] ?></option>
+                                                                    
                                                                     
                     <?php endforeach ?>
                   </select>
